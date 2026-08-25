204beb928044535c22b44a49e4d8cdb4e23d83d7
Marlen <meaguilar@uca.edu.sv>
2026-08-25 16:47:29 -0600

    feat: agregar ejercicio sobre el uso de funciones y sobrecarga de funciones

--- calcular_area_rectangulo.cc
+++ calcular_area_rectangulo.cc
@@ -1,35 +1,52 @@
 #include <iostream>
 
-// Declaracion
+// Declaracion de las funciones 
 void SolicitarDatos();
 double CalcularSuperficie(double base, double altura);
-void Imprimir(double superficie); 
+void Imprimir(double superficie);
+void Imprimir();
 
+// Ambito global
+double base;   
+double altura; 
 
 int main()
 {
-    // Llamado a la funcion 
-    
+    double superficie = 0;
+
+    // Llamado a la funcion de usuario
+    SolicitarDatos();
+    superficie = CalcularSuperficie(base, altura);
+    Imprimir(superficie);
+
+    // Funciones internas 
+    std::cout<< "Peso en bytes de double:" << sizeof(base);
 
     return 0;
 }
 
-// Definicion
-void SolicitarDatos(){
-    // Variables de ambito local
-    double base; 
-    double altura; 
-
-    std::cout<<"Ingresar base:";
-    std::cin>>base;
-    std::cout<<"Ingresar base:";
-    std::cin>>altura;
+// Definicion de las funciones
+void SolicitarDatos()
+{
+    std::cout << "Ingresar base:";
+    std::cin >> base;
+    std::cout << "Ingresar base:";
+    std::cin >> altura;
 }
 
-double CalcularSuperficie(double base, double altura){
+double CalcularSuperficie(double base, double altura)
+{
     return base * altura;
 }
 
-void Imprimir(double superficie){
-    std::cout<<"La superficie calculada: " << superficie;
+// Sobrecarga de funciones 
+
+void Imprimir(double superficie)
+{
+    std::cout << "La superficie calculada: " << superficie << "/n";
+}
+
+void Imprimir(){
+
+    std::cout<<"Probando la sobrecarga de funciones en C++";
 }
